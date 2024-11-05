a95bbc226eb7128b2502a3b32c4840afd331ef8b
Denis Almeida <denisjosealmeida@gmail.com>
2024-11-05 13:01:07 -0300

    Limpeza do repositório.

--- assets/scripts/script.js
+++
@@ -1,7 +0,0 @@
-let authorsData;
-
-(async () => {
-  const res = await fetch("/assets/dados/autores.json");
-  authorsData = await res.json();
-  console.log(authorsData.authors[0]["author-name"]);
-})();
